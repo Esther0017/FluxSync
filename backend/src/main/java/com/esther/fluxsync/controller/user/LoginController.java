216a0165439dfca39821862acd2346d4e95d8488
Esther <3363698991@qq.com>
2025-09-17 20:28:53 +0800

    feat(application): 添加基础路由支持
    - 修改 application 文件以完善路由结构
    - 完成 /health 健康检查路由
    - 初始化 /user/login 与 /user/register 用户认证路由

---
+++ backend/src/main/java/com/esther/fluxsync/controller/user/LoginController.java
@@ -0,0 +1,8 @@
+package com.esther.fluxsync.controller.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class LoginController {
+
+}
---
+++ backend/src/main/java/com/esther/fluxsync/controller/user/RegisterController.java
@@ -0,0 +1,14 @@
+package com.esther.fluxsync.controller.user;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+class RegisterController {
+
+//    @PostMapping("/register")
+//    public String register(@RequestBody User user) {
+//
+//    }
+
+}
