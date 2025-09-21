bb195d6e4854f317846563b96b0d2d3ecbea48a8
Esther <3363698991@qq.com>
2025-09-21 12:04:20 +0800

    feat(db): 优化用户类型表结构
    - 调整 SQL 用户类型表结构
    - 提升数据一致性与可维护性

--- mysql/init.sql
+++ mysql/init.sql
@@ -8,13 +8,13 @@ CREATE TABLE users
 (
     uid      INT          NOT NULL AUTO_INCREMENT COMMENT '用户唯一标识符',
     username VARCHAR(255) NOT NULL COMMENT '用户名',
-    nickname  VARCHAR(255) NOT NULL COMMENT '用户名称',
+    nickname VARCHAR(255) COMMENT '用户名称',
     password CHAR(32)     NOT NULL COMMENT '用户密码',
     regtime  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '注册时间',
     role     ENUM('admin', 'normal')  NOT NULL DEFAULT 'normal' COMMENT '用户级别',
     PRIMARY KEY (uid),
     UNIQUE KEY uk_user_username (username)
-) ENGINE=InnoDB AUTO_INCREMENT=10001 DEFAULT CHARSET=utf8 COMMENT='用户信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=10001 DEFAULT CHARSET=utf8mb4 COMMENT='用户信息表';
 
 CREATE TABLE teams
 (
@@ -24,7 +24,7 @@ CREATE TABLE teams
     createtime  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     PRIMARY KEY (tid),
     UNIQUE KEY uk_teams_name (name)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='团队信息表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='团队信息表';
 
 CREATE TABLE teams_users
 (
@@ -42,4 +42,15 @@ CREATE TABLE teams_users
         FOREIGN KEY (tid) REFERENCES teams (tid)
             ON DELETE CASCADE
             ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='团队成员关联表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='团队成员关联表';
+
+CREATE TABLE totp_info
+(
+    username    VARCHAR(255) NOT NULL COMMENT '用户名',
+    totp_secret VARCHAR(32)  NOT NULL COMMENT 'totp密钥',
+    totp_enable BOOLEAN DEFAULT FALSE COMMENT '是否开启totp校验',
+    PRIMARY KEY (username),
+    FOREIGN KEY (username) REFERENCES users (username)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='totp信息表';
